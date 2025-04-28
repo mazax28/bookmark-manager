79da263924a5929b1ed060aa543f815c839f909b
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-04-27 22:02:19 -0300

    feat: add VerificationPage component to display verification form

---
+++ src/pages/VerificationPage.jsx
@@ -0,0 +1,11 @@
+import VerifyCodeForm from "../components/VerifyCodeForm"
+function VerificationPage() {
+  return (
+    <div className='max-w-xs mx-auto bg-base-200 border-base-300 rounded-box border p-4'>
+
+      <VerifyCodeForm />
+    </div>
+  )
+}
+
+export default VerificationPage
