e28346f5db8703cded8390aa8a7c128c42518985
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-04-27 22:02:34 -0300

    feat: create HomePage component for the homepage layout

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <div>
+        <h1 className='text-3xl text-center'>Bienvenido a la página de inicio</h1>
+    </div>
+  )
+}
+
+export default HomePage
