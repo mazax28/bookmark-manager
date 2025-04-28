61bc23951cea7baf68d0f5111ac3c31ab5f8c982
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-04-27 22:02:43 -0300

    feat: implement routing for authentication and verification pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import AuthPage from "./pages/AuthPage"
+import HomePage from "./pages/HomePage"
+import VerificationPage from "./pages/VerificationPage"
+import { Toaster } from "react-hot-toast"
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { Routes, Route } from "react-router-dom"
+
 
 function App() {
 
   return (
     <div className="min-h screen">
-     
+      <Routes>
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/verify-email" element={<VerificationPage />} />
+
+        </Routes>
+      <Toaster/>
     </div>
   )
 }
