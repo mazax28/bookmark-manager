e1fc81c03f1d7e4945699d4d26d32558ea54c206
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-04-29 16:06:29 -0300

    feat: add BookmarkHeader component for managing bookmarks and folder views

---
+++ src/components/BookmarkHeader.jsx
@@ -0,0 +1,27 @@
+import AddDropdown from "./AddDropdown"
+import BookmarkFilter from "./BookmarkFilter"
+import BookmarkModal from "./BookmarkModal"
+import FolderModal from "./FolderModal"
+import ViewSelector from "./ViewSelector"
+function BookmarkHeader() {
+  return (
+    <div className='flex items-center justify-between bg-base-200 px-4 py-2 rounded-box'>
+        <div className='flex items-center gap-4'>
+            <input type="text" placeholder="Type here" className="input" />
+            {/* You can open the modal using document.getElementById('ID').showModal() method */}
+            <BookmarkModal />
+            <FolderModal />
+            <AddDropdown />
+
+        </div>
+        <div className="flex items-center gap-4">
+
+          <BookmarkFilter />
+          <ViewSelector />
+        </div>
+      
+    </div>
+  )
+}
+
+export default BookmarkHeader
