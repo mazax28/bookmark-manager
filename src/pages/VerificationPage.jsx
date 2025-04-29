08fa3f95f57547384a3d2de1cda6ba6b7b3bdd14
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-04-28 22:28:30 -0300

    refactor: remove unnecessary blank line in VerificationPage component

--- src/pages/VerificationPage.jsx
+++ src/pages/VerificationPage.jsx
@@ -2,7 +2,6 @@ import VerifyCodeForm from "../components/VerifyCodeForm"
 function VerificationPage() {
   return (
     <div className='max-w-xs mx-auto bg-base-200 border-base-300 rounded-box border p-4'>
-
       <VerifyCodeForm />
     </div>
   )
