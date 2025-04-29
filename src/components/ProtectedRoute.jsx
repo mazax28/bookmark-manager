476b9e864c741e6ebbaac286ebf866688e12f111
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-04-28 21:25:04 -0300

    feat: add ProtectedRoute component for authentication-based routing

---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,24 @@
+import { Navigate } from "react-router-dom";
+import { useAuthStore } from "../store/authStore";
+
+function ProtectedRoute({ children }) {
+  const { isAuthenticated, isCheckingAuth,user } = useAuthStore();
+
+  if (!isAuthenticated) {
+    // Si no está autenticado, lo redirige a login
+    return <Navigate to="/auth" replace />;
+  }
+  if (isAuthenticated && user.isverified){
+    return <Navigate to="/home" replace />;
+  }
+  if (isAuthenticated && !user.isverified){
+    return <Navigate to="/verify-email" replace />;
+  }
+
+  
+
+  // Si está autenticado, renderiza el componente hijo
+  return children;
+}
+
+export default ProtectedRoute;
