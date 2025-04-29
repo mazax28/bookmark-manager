3602d8b5ce540c8952dc62f7b3264a6d6b8c280b
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-04-29 16:06:55 -0300

    feat: create BookmarkPage component to display bookmarks and folders

---
+++ src/pages/BookmarkPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import BookmarkHeader from '../components/BookmarkHeader'
+import BookmarkCointainer from '../components/BookmarkCointainer'
+function BookmarkPage() {
+  return (
+    <div>
+        <BookmarkHeader />
+        <BookmarkCointainer />
+      
+    </div>
+  )
+}
+
+export default BookmarkPage
