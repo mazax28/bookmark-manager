06f58cea30188bce21ccd477f1cdf18c5fda324f
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-04-28 22:28:22 -0300

    refactor: remove isCheckingAuth state and related logic from authStore

--- src/store/authStore.js
+++ src/store/authStore.js
@@ -4,10 +4,9 @@ const useAuthStore = create((set) => ({
   user: null,
   isAuthenticated: false,
   error: null,
-  isCheckingAuth: true,
-  login: (user) => set({ user, isAuthenticated: true, error: null,isCheckingAuth:false}),
-  register: (user) => set({ user, error: null }),
-  verifyUser: (user) => set({user, isChekingAuth:false,isAuthenticated:true }),
+  login: (user) => set({ user, isAuthenticated: true, error: null}),
+  register: (user) => set({ user, error: null,isAuthenticated:true }),
+  verifyUser: (user) => set({user}),
   setError: (error) => set({ error, isAuthenticated: false }),
   logout: () => set({ user: null, isAuthenticated: false, error: null }),
 
