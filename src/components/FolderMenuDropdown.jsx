baefb48f6cf0de75674248ddd097389a90729e67
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-04-29 16:05:57 -0300

    feat: add FolderMenuDropdown component for managing bookmarks

---
+++ src/components/FolderMenuDropdown.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function FolderMenuDropdown() {
+  return (
+<div className="dropdown dropdown-end">
+  <div tabIndex={0} role="button" className="btn m-1">⋮</div>
+  <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+    <li><a>Editar</a></li>
+    <li><a>Eliminar</a></li>
+    <li><a>Agregar bookmark</a></li>
+    <li><a>Agregar a favoritos</a></li>
+
+
+  </ul>
+</div>
+  )
+}
+
+export default FolderMenuDropdown
