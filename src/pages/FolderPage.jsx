b1eb3914731dc891103da810aaadf71a55d147c1
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-05-01 22:16:16 -0300

    feat: create FolderPage component to display folder header and container

---
+++ src/pages/FolderPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import FolderContainer from '../components/FolderCointainer'
+import FolderHeader from '../components/FolderHeader'
+import DeleteModal from '../components/DeleteModal'
+function BookmarkPage() {
+  return (
+    <div>
+        <FolderHeader />
+        <FolderContainer />
+      
+    </div>
+  )
+}
+
+export default BookmarkPage
