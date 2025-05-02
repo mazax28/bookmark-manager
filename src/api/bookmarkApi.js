59f2ceebc595892141946b08012d12cacf60a914
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-05-02 12:02:44 -0300

    feat: implement bookmark API functions for CRUD operations

---
+++ src/api/bookmarkApi.js
@@ -0,0 +1,48 @@
+// src/api/authApi.js
+
+import axios from 'axios'; // o fetch, como prefieras
+axios.defaults.withCredentials = true; // Para enviar cookies con las peticiones
+
+const API_URL = 'http://localhost:8000/api/bookmarks'; // O ponerlo en env vars
+
+export async function getBookmarks() {
+  const response = await axios.get(`${API_URL}/`);
+  return response.data;
+}
+
+export async function addBookmark(data) {
+  const {title,description,url,folder} = data;
+  const response = await axios.post(`${API_URL}/`,{
+    title,
+    description,
+    url,
+    folder
+  }
+  );
+  return response.data;
+}
+export async function updateBookmark(data) {
+    const {id,title,description,url,folder} = data;
+    const response = await axios.put(`${API_URL}/${id}`,{
+      title,
+      description,
+      url,
+      folder
+    }
+    );
+    return response.data;
+  }
+
+export async function deleteBookmark(data) {
+    const {id} = data;
+    const response = await axios.delete(`${API_URL}/${id}`);
+    return response.data;
+}
+export async function moveBookmark(data) {
+    const {id,folder} = data;
+    const response = await axios.put(`${API_URL}/move/${id}`,{
+      folder
+    }
+    );
+    return response.data;
+}
