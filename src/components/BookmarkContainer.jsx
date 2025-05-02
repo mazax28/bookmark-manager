4b7ede6a3674e0037601721a269727d142325779
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-05-02 12:03:16 -0300

    feat: integrate bookmark fetching with API and handle loading and error states

--- src/components/BookmarkContainer.jsx
+++ src/components/BookmarkContainer.jsx
@@ -1,25 +1,45 @@
-import React from 'react'
-import { bookmarkmockData } from '../mock/mockData'
-import BookmarkCard from './BookmarkCard'
-import BookmarkTable from './BookmarkTable'
-import { useViewStore } from '../store/viewStore'
+import React from 'react';
+import { bookmarkmockData } from '../mock/mockData';
+import BookmarkCard from './BookmarkCard';
+import BookmarkTable from './BookmarkTable';
+import { useViewStore } from '../store/viewStore';
+import { useQuery } from '@tanstack/react-query';
+import { getBookmarks } from '../api/bookmarkApi';
 
 function BookmarkContainer() {
-  const { view } = useViewStore()
+  const { view } = useViewStore();
 
-  return (
-    view === 'grid' ? (
-      <div className="grid-fluid bg-base-100 p-4 rounded-box">
-        {bookmarkmockData.map((bookmark) => (
-          <BookmarkCard key={bookmark._id} bookmark={bookmark} />
-        ))}
+  const { data: bookmarks, isPending,isError,error } = useQuery({
+    queryKey: ['bookmarks'],
+    queryFn: getBookmarks,
+  });
+  console.log(bookmarks);
+
+  if (isPending) {
+    return <p className="p-4">Cargando bookmarks...</p>;
+  }
+  if (isError) {
+    return (
+      <div className="p-4 text-red-500">
+        Ocurrió un error al cargar los bookmarks: {error.message}
       </div>
-    ) : (
-      <div>
-        <BookmarkTable bookmarks={bookmarkmockData} />
-      </div>
-    )
-  )
+    );
+  }
+
+  // Fallback si no hay datos
+  const bookmarkData = bookmarks.bookmarks || bookmarkmockData;
+
+  return view === 'grid' ? (
+    <div className="grid-fluid bg-base-100 p-4 rounded-box">
+      {bookmarkData.map((bookmark) => (
+        <BookmarkCard key={bookmark._id} bookmark={bookmark} />
+      ))}
+    </div>
+  ) : (
+    <div>
+      <BookmarkTable bookmarks={bookmarkData} />
+    </div>
+  );
 }
 
-export default BookmarkContainer
+export default BookmarkContainer;
