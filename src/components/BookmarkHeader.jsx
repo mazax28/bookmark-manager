fd81fb8377bb628f838506411cfc83732eabd71b
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-05-03 15:45:04 -0300

    feat: implement search functionality in BookmarkHeader component

--- src/components/BookmarkHeader.jsx
+++ src/components/BookmarkHeader.jsx
@@ -3,23 +3,29 @@ import BookmarkFilter from "./BookmarkFilter"
 import BookmarkModal from "./BookmarkModal"
 import FolderModal from "./FolderModal"
 import ViewSelector from "./ViewSelector"
+import useSearchStore from "../store/useSearchStore"
+
 function BookmarkHeader() {
+  const { searchTerm, setSearchTerm } = useSearchStore();
+  
   return (
     <div className='flex items-center justify-between bg-base-200 px-4 py-2 rounded-box'>
         <div className='flex items-center gap-4'>
-            <input type="text" placeholder="Type here" className="input" />
-            {/* You can open the modal using document.getElementById('ID').showModal() method */}
+            <input 
+              type="text" 
+              placeholder="Buscar bookmarks..." 
+              className="input" 
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
             <BookmarkModal />
             <FolderModal />
             <AddDropdown />
-
         </div>
         <div className="flex items-center gap-4">
-
           <BookmarkFilter />
           <ViewSelector />
         </div>
-      
     </div>
   )
 }
