4cf4a5f8f2869479c6879cd977a397ee649dc8b8
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-05-03 15:45:25 -0300

    feat: implement folder filtering based on search term in FolderContainer component

--- src/components/FolderCointainer.jsx
+++ src/components/FolderCointainer.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
-import { foldermockData } from '../mock/mockData'
 import FolderCard from './FolderCard'
 import { getFoldersWithBoomarks } from '../api/folderApi';
 import { useQuery } from '@tanstack/react-query'
+import useSearchStore  from '../store/useSearchStore';
 function FolderContainer() {
+  const { folderSearchTerm } = useSearchStore();
 
-  const { data: foldersWithBookmarks, isPending,isError,error } = useQuery({
+  const { data, isPending,isError,error } = useQuery({
     queryKey: ['foldersWithBookmarks'],
     queryFn: getFoldersWithBoomarks,
   });
+  console.log(data)
+  console
   if (isPending) {
     return <p className="p-4">Cargando carpetas...</p>;
   }
@@ -19,15 +22,30 @@ function FolderContainer() {
       </div>
     );
   }
+
+  const filteredFolders = folderSearchTerm
+    ? data.filter((folder) => 
+        folder.name.toLowerCase().includes(folderSearchTerm.toLowerCase())
+      )
+    : data;
+  
+  // Si no hay resultados después de filtrar
+  if (filteredFolders.length === 0 && folderSearchTerm) {
+    return (
+      <div className="p-4 text-center">
+        No se encontraron carpetas con el término: <strong>{folderSearchTerm}</strong>
+      </div>
+    );
+  }
   // Fallback si no hay datos
-  const folderData = foldersWithBookmarks || foldermockData;
   return (
     <div className='bg-base-100 gap-4 grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 p-4 rounded-box '>
         {
-            folderData.map((folder) => (
+            filteredFolders?.map((folder) => (
                 <FolderCard key={folder._id} folder={folder} />
             ))
         }
+        
         
         
       
