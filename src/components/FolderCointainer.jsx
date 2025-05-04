f3964b07bb83ee4e9e6d2424b0c11ba32916f901
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-05-04 16:03:47 -0300

    refactor: clean up FolderContainer component by improving formatting and logging

--- src/components/FolderCointainer.jsx
+++ src/components/FolderCointainer.jsx
@@ -3,18 +3,21 @@ import FolderCard from './FolderCard'
 import { getFoldersWithBoomarks } from '../api/folderApi';
 import { useQuery } from '@tanstack/react-query'
 import useSearchStore  from '../store/useSearchStore';
+
 function FolderContainer() {
   const { folderSearchTerm } = useSearchStore();
 
-  const { data, isPending,isError,error } = useQuery({
+  const { data, isPending, isError, error } = useQuery({
     queryKey: ['foldersWithBookmarks'],
     queryFn: getFoldersWithBoomarks,
   });
-  console.log(data)
-  console
+  
+  console.log("Folders data:", data);
+  
   if (isPending) {
     return <p className="p-4">Cargando carpetas...</p>;
   }
+  
   if (isError) {
     return (
       <div className="p-4 text-red-500">
@@ -37,18 +40,12 @@ function FolderContainer() {
       </div>
     );
   }
-  // Fallback si no hay datos
+  
   return (
-    <div className='bg-base-100 gap-4 grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 p-4 rounded-box '>
-        {
-            filteredFolders?.map((folder) => (
-                <FolderCard key={folder._id} folder={folder} />
-            ))
-        }
-        
-        
-        
-      
+    <div className='bg-base-100 gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-4 rounded-box'>
+      {filteredFolders?.map((folder) => (
+        <FolderCard key={folder._id} folder={folder} />
+      ))}
     </div>
   )
 }
