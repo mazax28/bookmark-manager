778722ee2b0917a5a1cb6d91601fc174b3809b78
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-05-05 13:36:10 -0300

    fix: remove unused useEffect import in BookmarkContainer

--- src/components/BookmarkContainer.jsx
+++ src/components/BookmarkContainer.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import BookmarkCard from './BookmarkCard';
 import BookmarkTable from './BookmarkTable';
 import { useViewStore } from '../store/viewStore';
