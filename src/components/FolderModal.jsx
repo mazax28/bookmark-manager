21915d56dbd2649d60c26b79ad8080be5a15c6b3
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-05-05 22:42:44 -0300

    feat: implement folder creation functionality in FolderModal component

--- src/components/FolderModal.jsx
+++ src/components/FolderModal.jsx
@@ -1,6 +1,46 @@
 import React from 'react';
+import { useState } from 'react';
+import { toast } from 'react-hot-toast';
+import { addFolder } from '../api/folderApi';
+import { useMutation } from '@tanstack/react-query';
 
 function FolderModal() {
+
+  const [newFolder, setNewFolder] = useState({
+    folderName: '',
+    folderColor: ''
+  });
+
+  const {mutate, isPending} = useMutation({
+    mutationFn: addFolder,
+    onSuccess: () => {
+      toast.success('¡Carpeta creada correctamente!');
+      document.getElementById('folder_modal').close();
+      setNewFolder({
+        folderName: '',
+        folderColor: ''
+      });
+    },
+    onError: (error) => {
+      toast.error(`Error al crear la carpeta: ${error.message}`);
+    }
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    mutate({
+      name: newFolder.folderName,
+      color: newFolder.folderColor
+    });
+  }
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setNewFolder({
+      ...newFolder,
+      [id]: value
+    });
+  }
   return (
     <dialog id="folder_modal" className="modal">
       <div className="modal-box">
@@ -12,20 +52,22 @@ function FolderModal() {
           ✕
         </button>
 
-        <form className="fieldset space-y-4">
+        <form onSubmit={handleSubmit} className="fieldset space-y-4">
           <legend className="fieldset-legend mb-4 text-lg font-bold">Crear una carpeta</legend>
 
           <label className="label" htmlFor="folderName">Nombre de la carpeta</label>
           <input
             id="folderName"
             type="text"
+            onChange={handleChange}
+            value={newFolder.folderName}
             className="input w-full"
             placeholder="Ej: Trabajo, Tutoriales, Recursos"
             required
           />
 
           <label className="label" htmlFor="folderColor">Color (opcional)</label>
-          <select id="folderColor" defaultValue="" className="select w-full">
+          <select value={newFolder.folderColor} onChange={handleChange} id="folderColor" defaultValue="" className="select w-full">
             <option disabled value="">Elige un color</option>
             <option value="red">Rojo</option>
             <option value="blue">Azul</option>
