6e94d66a85e7ff8e7b0be29a091a8a076e2cb618
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-05-07 21:59:25 -0300

    style: update BookmarkPage layout with responsive container classes

--- src/pages/BookmarkPage.jsx
+++ src/pages/BookmarkPage.jsx
@@ -2,7 +2,7 @@ import BookmarkHeader from "../components/BookmarkHeader"
 import BookmarkContainer from "../components/BookmarkContainer"
 function BookmarkPage() {
   return (
-    <div>
+    <div className="max-w-7xl mx-auto px-4 py-6 space-y-6">
         <BookmarkHeader/>
         <BookmarkContainer/>
       
