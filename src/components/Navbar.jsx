f226c1aef8ccb32b71168743d71ee8faa618a0bb
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-06-17 14:43:55 -0300

    feat: refactor Navbar component to simplify dropdown structure and improve code readability

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,36 +32,25 @@ function Navbar() {
   }
   return (
 <div className="navbar bg-base-100 shadow-sm">
-    <div className="navbar-start">
-        {/* comentado por ahora */}
-        {/* <div className="dropdown">
-        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
-        </div>
-        <ul
+     <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /> </svg>
+          </div>
+          <ul
             tabIndex={0}
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-            <li><a>Item 1</a></li>
-            <li>
-            <a>Parent</a>
-            <ul className="p-2">
-                <li><a>Submenu 1</a></li>
-                <li><a>Submenu 2</a></li>
-            </ul>
-            </li>
-            <li><a>Item 3</a></li>
-        </ul>
-        </div> */}
-        <a className="btn btn-ghost text-xl">BookmarkHub</a>
-    </div>
+            <li><Link to="bookmarks">Bookmarks</Link></li>
+            <li><Link to="folders">Folders</Link></li>
+            <li><Link to="dashboard">Dashboard</Link></li>
+          </ul>
+        </div>
+        <a className="btn btn-ghost text-xl">BookmakHub</a>
+
+      </div>
+  
     {/* Comentado por ahora */}
-    <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-        <li><Link to="bookmarks">Bookmarks</Link></li>
-        <li><Link to="folders">Folders</Link></li>
-        <li><Link to="dashboard">Dashboard</Link></li>
-        </ul>
-    </div>
+    
     <div className="navbar-end space-x-4">
       <ThemeController />
     <div className="dropdown dropdown-end">
