dba252288d8f5a1ae078369c9811645a8ab05054
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-06-17 22:14:30 -0300

    fix: refactor authStore to use a function for isAuthenticated and remove isAuthenticated state

--- src/store/authStore.js
+++ src/store/authStore.js
@@ -1,15 +1,17 @@
 import { create } from "zustand";
 
-const useAuthStore = create((set) => ({
+const useAuthStore = create((set, get) => ({
   user: null,
-  isAuthenticated: false,
   error: null,
-  login: (user) => set({ user, isAuthenticated: true, error: null}),
-  register: (user) => set({ user, error: null,isAuthenticated:true }),
-  verifyUser: (user) => set({user}),
-  setError: (error) => set({ error, isAuthenticated: false }),
-  logout: () => set({ user: null, isAuthenticated: false, error: null }),
+  // Computar isAuthenticated basado en la existencia del usuario
+  isAuthenticated: () => !!get().user,
+  // Computar isVerified basado en el campo isverified del usuario
+  // isVerified: () => get().user?.isverified || false,
+  login: (user) => set({ user, error: null }),
+  register: (user) => set({ user, error: null }),
+  // verifyUser: (user) => set({ user }),
+  setError: (error) => set({ error }),
+  logout: () => set({ user: null, error: null }),
+}));
 
-}))
-
-export { useAuthStore }
+export { useAuthStore };
