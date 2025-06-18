1ccd0a9983f2609a5228125020be7cd325634dd4
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-06-17 22:17:43 -0300

    fix: simplify ProtectedRoute component by removing unnecessary user verification logic

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,21 +1,15 @@
-import  { useAuthStore } from "../store/authStore";
+import { useAuthStore } from "../store/authStore";
 import { Navigate } from "react-router-dom";
+
 function ProtectedRoute({ children }) {
-  const { isAuthenticated, user} = useAuthStore();
-
-
+  const isAuthenticated = useAuthStore(state => state.isAuthenticated());
+  
+  // Solo verificamos si el usuario está autenticado
   if (!isAuthenticated) {
     return <Navigate to="/auth" replace />;
   }
 
-  if (isAuthenticated && user && !user.isverified) {
-    return <Navigate to="/verify-email" replace />;
-  }
-  if (isAuthenticated && user && user.isverified) {
-    return <Navigate to="/home" replace />;
-  }
-
-  // Todo bien, renderiza lo que corresponda
+  // Si está autenticado, mostramos el contenido
   return children;
 }
 
